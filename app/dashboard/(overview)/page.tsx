4a1ef200da0d1b0cdaf8ef3fc4cd75e1fbe0ba82
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-06 19:15:52 +0000

    🚚 Moved page to new route group

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,23 +4,23 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import { Card } from '@/app/ui/dashboard/cards'
 import { lusitana } from '@/app/ui/fonts'
 
+import { fetchCardData } from '@/app/lib/data'
+
 import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from '@/app/lib/data'
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons'
+import { Suspense } from 'react'
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
+  // const revenue = await fetchRevenue()
+  // const latestInvoices = await fetchLatestInvoices()
   const {
     totalPaidInvoices,
     totalPendingInvoices,
     numberOfCustomers,
     numberOfInvoices,
   } = await fetchCardData()
-
-  console.log(latestInvoices)
 
   return (
     <main>
@@ -38,8 +38,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   )
